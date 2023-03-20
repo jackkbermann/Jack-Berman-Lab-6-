80f7eabdebeb8f766e98fcaadfbf04409046c46f
Jack Berman <jackberman@ufl.edu>
2023-03-20 17:01:11 -0400

    this is my encode and main function added to the repo

---
+++ main.py
@@ -0,0 +1,27 @@
+def encode(user_password):  # encodes user password and adds 3 to each value place in the string
+    encoded_password = ""
+    for value in user_password:
+        new_digit = str((int(value) + 3) % 10)
+        encoded_password = encoded_password + new_digit
+    return encoded_password
+
+
+def main():
+    while True:
+        print("Menu\n----------\n1.  Encode\n2.  Decode\n3.  Quit")  # prints menu for program
+        print()
+        user_input = int(input("Please enter an option: "))
+
+        if user_input == 1:  # runs the encode function and stores its value
+            user_password = (input("Please enter your password to encode: "))
+            encode(user_password)
+            print("Your password has been encoded and stored!")
+            print()
+        elif user_input == 2:  # returns original and encoded password
+            pass
+        elif user_input == 3:  # quits the program
+            break
+
+
+if __name__ == "__main__":
+    main()
