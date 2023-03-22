391f20bab6d178e5f7a1167388e9e92aac687036
Andrew Marquette <andrewmarquette@Andrews-MacBook-Pro.local>
2023-03-21 20:41:59 -0400

    Added the decode method

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def encode(new_password):  # Jack Berman
+def encode(password):  # Jack Berman
     new_password = ""  # encodes user password and adds 3 to each value place in the string
-    for value in new_password:
+    for value in password:
         new_digit = str((int(value) + 3) % 10)
         new_password = new_password + new_digit
     return new_password
 
+def decode(code):
+    orig_password = ""
+    array = [int(i) for i in str(code)]
+    for i in array:
+        i -= 3
+        a = str(i)
+        orig_password += a
+    return orig_password
 
 def main():
     while True:
@@ -13,12 +21,14 @@ def main():
         user_input = int(input("Please enter an option: "))
 
         if user_input == 1:  # runs the encode function and stores its value
-            new_password = (input("Please enter your password to encode: "))
-            encode(new_password)
+            password = (input("Please enter your password to encode: "))
+            code = encode(password)
             print("Your password has been encoded and stored!")
             print()
         elif user_input == 2:  # returns original and encoded password
-            pass
+            orig_password = decode(code)
+            print(f"The encoded password is {code} and the original password is {orig_password}")
+            print()
         elif user_input == 3:  # quits the program
             break
 
